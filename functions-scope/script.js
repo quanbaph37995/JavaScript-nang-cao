4a18bf5fb02c44663c8d2de4cddac108339c17e2
quanbaph37995 <quan261124@gmail.com>
2024-01-16 13:33:17 +0700

    init js

---
+++ functions-scope/script.js
@@ -0,0 +1,7 @@
+function sayHello(){
+    //k tham so, k return
+    // console.log("Hello cac ban toi la" + name);
+    console.log(`Hello cac ban toi la ${name}`);//noi chuoi js
+}
+
+sayHello(quan);
